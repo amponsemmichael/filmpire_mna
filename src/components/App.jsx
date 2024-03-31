1c7e0609a8787e5d1b853c19dd7ad240dc495be9
amponsem michael <amponsem_michael@yahoo.com>
2024-03-31 05:36:28 +0000

    committing changes to the movie app after learning how to debug with eslint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './index';
 
 const App = () => (
   <div>
-    <h1>Welcome, to filmPire App</h1>
+    <CssBaseline />
+    <NavBar />
+    <main>
+      <Switch>
+        <Route exact path="/movie/:id">
+          <MovieInformation />
+        </Route>
+        <Route exact path="/actors/:id">
+          <Actors />
+        </Route>
+        <Route exact path="/movies/:id">
+          <Movies />
+        </Route>
+        <Route exact path="/profile/:id">
+          <Profile />
+        </Route>
+      </Switch>
+    </main>
   </div>
 );
+
 export default App;
